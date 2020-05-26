f96e8ac0ff566951c6c687a35ba2f19879e254a9
zhangruihua <zhangruihua@mininglamp.com>
2020-05-26 10:00:31 +0800

    添加post发送方法

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
 const puppeteer = require('puppeteer');
 const dayjs = require('dayjs')
 const axios = require('axios')
+const wxConfig = require('./config/wx')
 setInterval(async () => {
     let currentTime = dayjs(new Date()).format('HH')
-    let execTime = '10'
-    if (currentTime == execTime) {
-        console.log("开始执行任务...")
+    //每天10点以后执行一次
+    if (currentTime == '10') {
+        const browser = await puppeteer.launch({ headless: true });
+        const page = await browser.newPage();
+        await page.goto('https://nba.hupu.com/');
+        const newsUrl = await page.evaluate(() => {
+            let ele = document.getElementsByClassName('list-news')[0].children[0].children[0]
+            return {
+                url: ele.href
+            }
+        });
+        console.log('新闻内容url:', newsUrl.url);
+        await page.goto(newsUrl.url);
+        const newsContent = await page.evaluate(() => {
+            let ele = document.getElementsByClassName('quote-content')[0]
+            return {
+                content: ele.innerText
+            }
+        });
+        await browser.close();
+        let data = {
+            msgtype: 'text',
+            text: {
+                content: newsContent.content
+            }
+        }
+        console.log('新闻内容:', data)
+        axios.post(wxConfig.wxWebhook, data)
     }
-    console.log("正在获取新闻内容...", currentTime, execTime)
-    const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
-    await page.goto('https://nba.hupu.com/');
-    const newsUrl = await page.evaluate(() => {
-        let ele = document.getElementsByClassName('list-news')[0].children[0].children[0]
-        return {
-            url: ele.href
-        }
-    });
-    console.log('新闻内容url:', newsUrl.url);
-    await page.goto(newsUrl.url);
-    const newsContent = await page.evaluate(() => {
-        let ele = document.getElementsByClassName('quote-content')[0]
-        return {
-            content: ele.innerText
-        }
-    });
-    console.log('新闻内容:', newsContent)
-    await browser.close();
 }, 1000 * 60 * 60);
 
 
