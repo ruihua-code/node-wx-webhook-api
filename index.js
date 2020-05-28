0d3aed7741400c0a8fdac6daaa0e7b62ecc39c9b
zhangruihua <zhangruihua@mininglamp.com>
2020-05-28 14:23:03 +0800

    更新puppeteer方法

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const dayjs = require('dayjs')
 const axios = require('axios')
 const wxConfig = require('./config/wx')
 setInterval(async () => {
-    let currentTime = dayjs(new Date()).format('HH')
-    //每天10点以后执行一次
-    if (currentTime == '10') {
-        const browser = await puppeteer.launch({ headless: true });
-        const page = await browser.newPage();
+    let currentTime = dayjs(new Date()).format('HH:mm')
+    console.log(currentTime)
+    /*每天10点执行一次*/
+    if (currentTime == '10:00') {
+        const browser = await puppeteer.launch({ headless: false })
+        const browserWSEndpoint = browser.wsEndpoint();
+        browser.disconnect();
+        // 使用节点来重新建立连接
+        const browser2 = await puppeteer.connect({ browserWSEndpoint });
+        let page = await browser2.newPage()
         await page.goto('https://nba.hupu.com/');
         const newsUrl = await page.evaluate(() => {
             let ele = document.getElementsByClassName('list-news')[0].children[0].children[0]
@@ -15,7 +20,7 @@ setInterval(async () => {
                 url: ele.href
             }
         });
-        console.log('新闻内容url:', newsUrl.url);
+        console.log('url:', newsUrl.url)
         await page.goto(newsUrl.url);
         const newsContent = await page.evaluate(() => {
             let ele = document.getElementsByClassName('quote-content')[0]
@@ -23,17 +28,11 @@ setInterval(async () => {
                 content: ele.innerText
             }
         });
-        await browser.close();
-        let data = {
-            msgtype: 'text',
-            text: {
-                content: newsContent.content
-            }
-        }
-        console.log('新闻内容:', data)
-        axios.post(wxConfig.wxWebhook, data)
+        await browser2.close()
+        console.log('新闻内容:', newsContent)
+        axios.post(wxConfig.wxWebhook, newsContent)
     }
-}, 1000 * 60 * 60);
+}, 1000 * 60);
 
 
 
